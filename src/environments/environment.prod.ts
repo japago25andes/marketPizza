e56a5a053eb11575934077380a80f9d9f9afbec1
japago25andes <j.pardog@uniandes.edu.co>
2024-02-15 00:26:50 -0500

    Creando funcion de login con firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyDwqD8-qjDIAKC04O-Ji1tYzOtEew5djPI",
+    authDomain: "market-pizza-app.firebaseapp.com",
+    projectId: "market-pizza-app",
+    storageBucket: "market-pizza-app.appspot.com",
+    messagingSenderId: "528386712045",
+    appId: "1:528386712045:web:c60a5190a90d0ab2da47e4"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyDwqD8-qjDIAKC04O-Ji1tYzOtEew5djPI",
+    authDomain: "market-pizza-app.firebaseapp.com",
+    projectId: "market-pizza-app",
+    storageBucket: "market-pizza-app.appspot.com",
+    messagingSenderId: "528386712045",
+    appId: "1:528386712045:web:c60a5190a90d0ab2da47e4"
+  }
 };
 
 /*
