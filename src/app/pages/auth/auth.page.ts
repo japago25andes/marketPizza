8dd42465292dc7240d3f4cc78ebf04fd5b145ca4
japago25andes <j.pardog@uniandes.edu.co>
2024-02-15 00:35:36 -0500

    Servicio de utils y loading

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-auth',
@@ -16,14 +17,24 @@ export class AuthPage implements OnInit {
   });
 
   firebaseSvc = inject(FirebaseService);
+  utilsSvc = inject(UtilsService);
 
   ngOnInit() {
   }
 
-  submit() {
+  async submit() {
     if(this.form.valid){
+
+      const loading = await this.utilsSvc.loading();
+
+      await loading.present();
+
       this.firebaseSvc.signIn(this.form.value as User).then(res =>{
         console.log(res);
+      }).catch(error => {
+        console.log(error);
+      }).finally(() => {
+        loading.dismiss();
       })
     }
   }
---
+++ src/app/services/utils.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, inject } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilsService {
+
+  loadingCtrl = inject(LoadingController);
+
+  //=============== Loading ===============
+  loading(){
+    return this.loadingCtrl.create({ spinner: 'crescent'})
+  }
+}
