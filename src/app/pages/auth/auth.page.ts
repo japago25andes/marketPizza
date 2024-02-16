f65f29819ec6610763386515ba8cc6ffc40406f0
japago25andes <j.pardog@uniandes.edu.co>
2024-02-15 22:34:02 -0500

    Usando toast para validar errores de firebase

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -33,6 +33,15 @@ export class AuthPage implements OnInit {
         console.log(res);
       }).catch(error => {
         console.log(error);
+
+        this.utilsSvc.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'danger',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        });
+
       }).finally(() => {
         loading.dismiss();
       })
